41e7feb6a87e827fa65f3ed113a124f6d1737d8b
Hex3 <hexthree@protonmail.com>
2026-02-15 23:17:58 +0000

    init 1.0.0

---
+++ CollapseNerf/Main.cs
@@ -0,0 +1,58 @@
+using BepInEx;
+using R2API.Utils;
+using RoR2;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System.Reflection;
+using System;
+using BepInEx.Configuration;
+
+namespace CollapseNerf
+{
+    [BepInPlugin(COLLAPSENERF_GUID, COLLAPSENERF_NAME, COLLAPSENERF_VER)]
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string COLLAPSENERF_GUID = "com.Hex3.CollapseNerf";
+        public const string COLLAPSENERF_NAME = "CollapseNerf";
+        public const string COLLAPSENERF_VER = "1.0.0";
+        public static Main Instance;
+
+        public void Awake()
+        {
+            Log.Init(Logger);
+            Log.Info($"Init {COLLAPSENERF_NAME} {COLLAPSENERF_VER}");
+
+            Instance = this;
+
+            Log.Info($"Creating hooks...");
+
+            ConfigEntry<float> Collapse_MaxPercent = Instance.Config.Bind(new ConfigDefinition("Collapse nerf", "Max damage percentage"), 10f, new ConfigDescription("Max percentage of an ally's health that collapse can deal. Must be above 0.", null, Array.Empty<object>()));
+
+            On.RoR2.DotController.InflictDot_refInflictDotInfo += (orig, ref self) =>
+            {
+                if 
+                (
+                    self.dotIndex == DotController.DotIndex.Fracture
+                    && self.attackerObject 
+                    && self.attackerObject.TryGetComponent(out CharacterBody attackerBody)
+                    && self.victimObject.TryGetComponent(out CharacterBody victimBody)
+                    && victimBody.healthComponent
+                    && attackerBody.teamComponent 
+                    && (attackerBody.teamComponent.teamIndex == TeamIndex.Monster || attackerBody.teamComponent.teamIndex == TeamIndex.Void)
+                )
+                {
+                    float fractureDamage = attackerBody.baseDamage * 4f;
+                    float collapseLimit = (victimBody.healthComponent.fullCombinedHealth * (Collapse_MaxPercent.Value / 100f)) / 4f;
+                    
+                    self.totalDamage = Math.Min(fractureDamage, collapseLimit);
+                }
+
+                orig(ref self);
+            };
+
+            Log.Info($"Done");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# CollapseNerf
+
+Changes collapse on enemy entities so that it can no longer deal more than 10% (configurable) of your maximum health as damage.
+
+This is to counteract enemies with high fire rates dealing unfair levels of damage by inflicting many stacks of collapse at once. Enemies that inflict fewer stacks tend to hit harder anyway.
